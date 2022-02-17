0cd65b09e826c827efc4f6f4dac6bfa4cefdaa07
shehabAbouelhassan <55254824+shehabAbouelhassan@users.noreply.github.com>
2022-02-17 23:47:43 +0100

    first commit

---
+++ pdt/src/main/java/io/dewe/pdt/PdtApplication.java
@@ -0,0 +1,13 @@
+package io.dewe.pdt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdtApplication.class, args);
+    }
+
+}
---
+++ pdt/src/test/java/io/dewe/pdt/PdtApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dewe.pdt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
