7b01bfbf6756a51e6f8a29364adfd34f154a4e20
shehabAbouelhassan <55254824+shehabAbouelhassan@users.noreply.github.com>
2022-02-18 15:30:37 +0100

    adding front end

---
+++ pdt-react-client/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Dashboard from './components/Dashboard';
+import Header from './components/Layout/Header';
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <div ClassName=  "App">
+    <Header/>
+    <Dashboard/>
+    </div>
+  );
+}
+
+export default App;
---
+++ pdt-react-client/src/components/Dashboard.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+import Projectitem from "./Project/Projectitem";
+class Dashboard extends Component {
+    render() {
+        return (
+            <div className="projects">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <h1 className="display-4 text-center">Projects</h1>
+                        <br />
+                        <a href="ProjectForm.html" className="btn btn-lg btn-info">
+                            Create a Project
+                        </a>
+                        <br />
+                        <hr />
+    <Projectitem/>
+                        </div>
+                </div>
+            </div>
+        </div>
+            );
+    }
+}
+export default Dashboard;
