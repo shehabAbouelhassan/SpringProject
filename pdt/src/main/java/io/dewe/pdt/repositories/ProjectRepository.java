cfdb9a99fff2243a8de5e089102bb090b1de47ab
shehabAbouelhassan <55254824+shehabAbouelhassan@users.noreply.github.com>
2022-02-18 01:07:19 +0100

    second commit

--- pdt/src/main/java/io/dewe/pdt/repositories/ProjectRepository.java
+++ pdt/src/main/java/io/dewe/pdt/repositories/ProjectRepository.java
@@ -1,4 +1,11 @@
 package io.dewe.pdt.repositories;
 
-public interface ProjectRepository {
+import io.dewe.pdt.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> longs);
 }
--- pdt/src/main/java/io/dewe/pdt/services/ProjectService.java
+++ pdt/src/main/java/io/dewe/pdt/services/ProjectService.java
@@ -1,4 +1,19 @@
 package io.dewe.pdt.services;
 
+import io.dewe.pdt.domain.Project;
+import io.dewe.pdt.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project){
+
+        //Logic
+        return projectRepository.save(project);
+    }
 }
